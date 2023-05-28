bc713ba5ec9f85b076077e727abe571611af6502
franus <98951386+franu5@users.noreply.github.com>
2023-05-28 21:22:43 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,7 @@
-local f5bot = "https://discord.com/api/webhooks/1110553433397788732/Ec88ETsA1nEs41A89EsxQhMx3Q03mYrM132NcVcXvB0-AYdCPyLjfzNa54-kcuhkBYEk"
-
 AddEventHandler('playerConnecting', function()
-local argString = table.concat(args, " ")
-local name = GetPlayerName(source)
-local ip = GetPlayerEndpoint(source)
-local steamhex = GetPlayerIdentifier(source)
 
-MySQL.Async.fetchAll("INSERT INTO f5s (adressip, nick, steam) VALUES(@ip, @name, @steamhex)")
+    MySQL.Async.fetchAll("INSERT INTO f5s (adressip, nick, steam, connectdate, connecttime) VALUES(@ip, @name, @steamhex, @connectdate, @connecttime)",
+        {["@ip"] = GetPlayerEndpoint(source), ["@name"] = GetPlayerName(source), ["@steamhex"] = GetPlayerIdentifier(source), ["@connectdate"] = (os.date("%x")), ["@connecttime"] = (os.date("%X"))}
+    )
 
-local added = {
-        {
-            ["color"] = "8663711",
-            ["title"] = "informacje o **"..name.."** zostaly dodane do bazy danych - **f5s**"
-        }
-    }
-PerformHttpRequest(f5bot, function(err, text, headers) end, 'POST', json.encode({username = "f5bot", embeds = connect}), { ['Content-Type'] = 'application/json' })
 end)
